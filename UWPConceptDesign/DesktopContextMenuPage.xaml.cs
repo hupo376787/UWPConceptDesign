2a8a468e6f90eaf49b561b2c3f99f75c22b878fb
vincentzwang <vincent.z.wang@pccw.com>
2020-03-16 23:12:26 +0800

    Init first commit, including a concept desktop context menu.

---
+++ UWPConceptDesign/DesktopContextMenuPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPConceptDesign
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DesktopContextMenuPage : Page
+    {
+        public DesktopContextMenuPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += DesktopContextMenu_Loaded;
+        }
+
+        private void DesktopContextMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 100));
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+        }
+    }
+}
---
+++ UWPConceptDesign/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPConceptDesign
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent(); 
+            ApplicationView.PreferredLaunchViewSize = new Size(1280, 800);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(200, 100));
+        }
+
+        private async void DesktopContextMenu_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var currentAV = ApplicationView.GetForCurrentView();
+            //get the bounds of current window.
+            var rect = Window.Current.Bounds;
+            var newAV = CoreApplication.CreateNewView();
+            await newAV.Dispatcher.RunAsync(
+                            CoreDispatcherPriority.Normal,
+                            async () =>
+                            {
+                                var newWindow = Window.Current;
+                                var newAppView = ApplicationView.GetForCurrentView();
+                                //newAppView.Title = "New window";
+
+                                var frame = new Frame();
+                                frame.Navigate(typeof(DesktopContextMenuPage), null);
+                                newWindow.Content = frame;
+                                newWindow.Activate();
+
+                                await ApplicationViewSwitcher.TryShowAsStandaloneAsync(
+                                    newAppView.Id,
+                                    ViewSizePreference.UseHalf,
+                                    currentAV.Id,
+                                    ViewSizePreference.UseHalf);
+                                newAppView.TryResizeView(new Size { Width = rect.Width - 890, Height = rect.Height - 320 });
+                            });
+        }
+    }
+}
